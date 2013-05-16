9ecb836ccc805246b9ee18f59df41f4bd7539053
Fraser Scott <fraser.scott@gmail.com>
2013-05-16 15:23:25 +0000

    * Bug fix for re-setting variables * Adding fontcolor tests * Removed some permutations

--- test/test_ulg.rb
+++ test/test_ulg.rb
@@ -5,31 +5,24 @@ class ULGTest < Test::Unit::TestCase
 
   def test_all_the_things
 
-    a_nodes = [ "anode", "a node", " a node ", "  a node  " ]
-    b_nodes = [ "bnode", "b node", " b node ", "  b node  " ]
-    edge_labels = [ "none", "edge", "ed ge", " edge ", "  ed ge   " ]
+    a_nodes = [ "anode", "a node", "   a node ", "  a node  " ]
+    b_nodes = [ "bnode", "b node", "   b node ", "  b node  " ]
+    edge_labels = [ "none", "edge", "ed ge", "   edge ", "  ed ge   " ]
 
     colours = [ "red", "none" ]
 
     shapes = {
-      "box"     => "[]",
       "oval"    => "()",
-      "diamond" => "<>",
-      "hexagon" => "{}",
       "none"    => ""
     }
 
     lines = {
-      "dotted"  => ".",
       "dashed"  => "-",
       "solid"   => "="
     }
 
     arrows = {
       "normal"  => ">",
-      "dot"     => "o",
-      "box"     => "x",
-      "diamond" => "<>",
       "none"    => ""
     }
 
@@ -41,48 +34,73 @@ class ULGTest < Test::Unit::TestCase
 
     a_nodes.each do |anode|
       shapes.keys.each do |a_shape_name|
-        lines.keys.each do |line_name|
-          edge_labels.each do |edge_label|
-            b_nodes.each do |bnode|
-              shapes.keys.each do |b_shape_name|
+        colours.each do |a_fontcolor|
+          lines.keys.each do |line_name|
+            edge_labels.each do |edge_label|
+              b_nodes.each do |bnode|
+                shapes.keys.each do |b_shape_name|
+                  colours.each do |b_fontcolor|
+ 
+                    test_data[test_case] = {
+                      'nodes' => { 
+                        'anode' => { 'attr' => {} },
+                        'bnode' => { 'attr' => {} }
+                      },
+                      'edge' => { 'attr' => {} }
+                    }
 
-                test_data[test_case] = {
-                  'nodes' => { 
-                    'anode' => { 'attr' => {} },
-                    'bnode' => { 'attr' => {} }
-                  },
-                  'edge' => { 'attr' => {} }
-                }
+                    line = []
+                    line << shapes[a_shape_name][0] if a_shape_name != "none"
+                    line << anode
+                    line << "|"+a_fontcolor if a_fontcolor != "none"
+                    line << shapes[a_shape_name][1] if a_shape_name != "none"
+                    line << " "
+                    line << lines[line_name]
+                    line << edge_label if edge_label != "none"
+                    line << lines[line_name]
+                    line << ">"
+                    line << " "
+                    line << shapes[b_shape_name][0] if b_shape_name != "none"
+                    line << bnode
+                    line << "|"+b_fontcolor if b_fontcolor != "none"
+                    line << shapes[b_shape_name][1] if b_shape_name != "none"
 
-                line = []
-                line << shapes[a_shape_name][0] if a_shape_name != "none"
-                line << anode
-                line << shapes[a_shape_name][1] if a_shape_name != "none"
-                line << " "
-                line << lines[line_name]
-                line << edge_label if edge_label != "none"
-                line << lines[line_name]
-                line << ">"
-                line << " "
-                line << shapes[b_shape_name][0] if b_shape_name != "none"
-                line << bnode
-                line << shapes[b_shape_name][1] if b_shape_name != "none"
+                    test_data[test_case]['line'] = line.join
 
-                test_data[test_case]['line'] = line.join ""
+                    test_data[test_case]['nodes']['anode']['attr']['label'] = anode.rstrip.lstrip
+                    test_data[test_case]['nodes']['bnode']['attr']['label'] = bnode.rstrip.lstrip
 
-                # Set defaults
-                a_shape_name = "box" if a_shape_name == "none"
-                b_shape_name = "box" if b_shape_name == "none"
+                    if a_shape_name == "none"
+                      test_data[test_case]['nodes']['anode']['attr']['shape'] = "box"
+                    else
+                      test_data[test_case]['nodes']['anode']['attr']['shape'] = a_shape_name
+                    end
 
-                test_data[test_case]['nodes']['anode']['attr']['label'] = anode.rstrip.lstrip
-                test_data[test_case]['nodes']['anode']['attr']['shape'] = a_shape_name
-                test_data[test_case]['nodes']['bnode']['attr']['label'] = bnode.rstrip.lstrip
-                test_data[test_case]['nodes']['bnode']['attr']['shape'] = b_shape_name
+                    if b_shape_name == "none"
+                      test_data[test_case]['nodes']['bnode']['attr']['shape'] = "box"
+                    else
+                      test_data[test_case]['nodes']['bnode']['attr']['shape'] = b_shape_name
+                    end
 
-                test_data[test_case]['edge']['attr']['style'] = line_name
-                test_data[test_case]['edge']['attr']['label'] = edge_label.rstrip.lstrip if edge_label != "none"
+                    if a_fontcolor == "none"
+                      test_data[test_case]['nodes']['anode']['attr']['fontcolor'] = "black"
+                    else
+                      test_data[test_case]['nodes']['anode']['attr']['fontcolor'] = a_fontcolor
+                    end
 
-                test_case += 1
+                    if b_fontcolor == "none"
+                      test_data[test_case]['nodes']['bnode']['attr']['fontcolor'] = "black"
+                    else
+                      test_data[test_case]['nodes']['bnode']['attr']['fontcolor'] = b_fontcolor
+                    end
+
+                    test_data[test_case]['edge']['attr']['style'] = line_name
+                    test_data[test_case]['edge']['attr']['label'] = edge_label.rstrip.lstrip if edge_label != "none"
+
+                    test_case += 1
+
+                  end
+                end
               end
             end
           end
